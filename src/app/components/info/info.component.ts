ad9abccdfb1bb082aa566fa1f612d3204b2d02fb
Z50 <jantsek.zsolt@gmail.com>
2025-11-28 15:22:32 +0100

    info card update

--- src/app/components/info/info.component.ts
+++ src/app/components/info/info.component.ts
@@ -1,16 +1,28 @@
 import { Component } from '@angular/core';
 import { TRANSPORT_MODE } from '../../shared/constants/transport-mode';
 import { CommonModule, KeyValuePipe } from '@angular/common';
+import { NzSegmentedModule } from 'ng-zorro-antd/segmented';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'info',
   imports: [
     CommonModule,
-    KeyValuePipe
+    KeyValuePipe,
+    NzSegmentedModule,
+    FormsModule,
   ],
   templateUrl: './info.component.html',
   styleUrl: './info.component.scss'
 })
 export class InfoComponent {
-    public transportMode = TRANSPORT_MODE as Record<string, { name: string; label: string, icon: string }>;
+  public transportMode = TRANSPORT_MODE as Record<string, { name: string; label: string, icon: string }>;
+
+  infoIndex: string | number = 0;
+
+  tabs = [
+    { value: 0, label: 'User guide' },
+    { value: 1, label: 'Known issues' },
+    { value: 2, label: 'Legend' },
+  ];
 }
